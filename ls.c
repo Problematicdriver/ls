c5570829b9ff4d6c0a731ea0d7ea84efe6387ab9
Problematicdriver <jiayiguo@ip-172-31-85-108.ec2.internal>
2022-10-26 04:19:53 +0000

    get maxes

--- ls.c
+++ ls.c
@@ -17,6 +17,9 @@
 #include <pwd.h>
 #include <grp.h>
 
+#include "ls.h"
+
+#define NO_PRINT 1
 
 #define	BY_NAME 0
 #define	BY_SIZE 1
@@ -24,18 +27,92 @@
 
 #define BUFF_SIZE 512
 #define PERM_SIZE 12
+#define ID_SIZE 12
 
 int f_recursive;
 int f_seeAll;
 int f_seeHidden;
 int f_long;
+int f_inode;
+int f_type;
+int f_readable;
+int f_kilo;
+int f_numeric;
+int f_noprint;
+int f_reverse;
+int f_size;
+int f_block;
+int f_mtime;
+int f_atime;
+int f_raw;
 
 int fts_options;
 
 void
-display(const FTSENT *file)
+display(FTSENT *chp)
 {
+    /*
+     * find the maximum length for
+     * inode, nlinks, oname, gname, uid, gid 
+     */
+    
+    struct stat *sp;
+
+    FTSENT *curr;
+
+    off_t maxsize;
+    blkcnt_t maxblock;
+    ino_t maxinode;
+    uint32_t maxnlink;
+    int maxlen, max_oname, max_gname, max_uid, max_gid;
+    int olen, glen, nolen, nglen;
+    char uid[ID_SIZE], gid[ID_SIZE];
+    const char *owner, *group;
+
+    for (curr = chp; curr; curr = curr->fts_link) {
+        if (curr->fts_info == FTS_ERR) {
+            curr->fts_number = NO_PRINT;
+            continue;
+        }
+
+        if (sp->st_size > maxsize)
+			maxsize = sp->st_size;
         
+        if (sp->st_blocks > maxblock)
+			maxblock = sp->st_blocks;
+        
+        if (sp->st_ino > maxinode)
+			maxinode = sp->st_ino;
+        
+        if (sp->st_nlink > maxnlink)
+			maxnlink = sp->st_nlink;
+
+        if (curr->fts_namelen > maxlen)
+			maxlen = curr->fts_namelen;
+        
+        owner = user_from_uid(sp->st_uid, 0);
+
+        if ((olen = strlen(owner)) > max_oname)
+			max_oname = olen;
+
+        group = group_from_gid(sp->st_gid, 0);
+
+        if ((glen = strlen(group)) > max_gname)
+            max_gname = glen;
+
+        (void)snprintf(uid, sizeof(uid),"%u", sp->st_uid);
+        
+        if ((nolen = strlen(uid)) > max_uid) {
+            max_uid = nolen;
+        }
+        
+        (void)snprintf(gid, sizeof(gid),"%u", sp->st_gid);
+
+        if ((nglen = strlen(gid)) > max_gid) {
+            max_gid = nglen;
+        }
+        
+    }    
 }
 
 int
@@ -54,7 +131,7 @@ main(int argc, char **argv)
     FTS *ftsp;
 	FTSENT *p, *chp;
 	int ch_options, error;
-
+    
     if ((ftsp = fts_open(argv, fts_options, NULL)) == NULL) {
         err(EXIT_FAILURE, "fts_open");
     } 
---
+++ ls.h
@@ -0,0 +1,18 @@
+#define NO_PRINT 1
+
+extern int f_recursive;
+extern int f_seeAll;
+extern int f_seeHidden;
+extern int f_long;
+extern int f_inode;
+extern int f_type;
+extern int f_readable;
+extern int f_kilo;
+extern int f_numeric;
+extern int f_noprint;
+extern int f_reverse;
+extern int f_size;
+extern int f_block;
+extern int f_mtime;
+extern int f_atime;
+extern int f_raw;
